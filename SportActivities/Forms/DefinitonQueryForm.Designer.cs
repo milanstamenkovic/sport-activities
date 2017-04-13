070091b41a20194b8cc498b1f5255dc931a101e7
milanstamenkovic <milanstamenkovic@live.com>
2017-04-13 18:46:47 +0200

    spatial query initial

--- SportActivities/Forms/DefinitonQueryForm.Designer.cs
+++ SportActivities/Forms/DefinitonQueryForm.Designer.cs
@@ -34,47 +34,57 @@
             this.comboBoxLayer = new System.Windows.Forms.ComboBox();
             this.btnFilter = new System.Windows.Forms.Button();
             this.panelQuery = new System.Windows.Forms.Panel();
+            this.btnAddCondition = new System.Windows.Forms.Button();
+            this.panelQuery2 = new System.Windows.Forms.Panel();
+            this.btnAddCondition2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.relationComboBox = new System.Windows.Forms.ComboBox();
+            this.spatialQueryCheckBox = new System.Windows.Forms.CheckBox();
             this.panelQuery.SuspendLayout();
+            this.panelQuery2.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBoxAttributes
             // 
             this.comboBoxAttributes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxAttributes.FormattingEnabled = true;
-            this.comboBoxAttributes.Location = new System.Drawing.Point(134, 16);
+            this.comboBoxAttributes.Location = new System.Drawing.Point(130, 0);
             this.comboBoxAttributes.Name = "comboBoxAttributes";
             this.comboBoxAttributes.Size = new System.Drawing.Size(121, 21);
-            this.comboBoxAttributes.TabIndex = 0;
+            this.comboBoxAttributes.TabIndex = 1;
             // 
             // comboBoxQuery
             // 
             this.comboBoxQuery.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxQuery.FormattingEnabled = true;
-            this.comboBoxQuery.Location = new System.Drawing.Point(273, 16);
+            this.comboBoxQuery.Location = new System.Drawing.Point(270, 0);
             this.comboBoxQuery.Name = "comboBoxQuery";
-            this.comboBoxQuery.Size = new System.Drawing.Size(81, 21);
+            this.comboBoxQuery.Size = new System.Drawing.Size(80, 21);
             this.comboBoxQuery.TabIndex = 1;
             // 
             // textBoxValue
             // 
-            this.textBoxValue.Location = new System.Drawing.Point(372, 16);
+            this.textBoxValue.Location = new System.Drawing.Point(370, 0);
             this.textBoxValue.Name = "textBoxValue";
-            this.textBoxValue.Size = new System.Drawing.Size(155, 20);
+            this.textBoxValue.Size = new System.Drawing.Size(130, 20);
             this.textBoxValue.TabIndex = 2;
             // 
             // comboBoxLayer
             // 
             this.comboBoxLayer.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxLayer.FormattingEnabled = true;
-            this.comboBoxLayer.Location = new System.Drawing.Point(3, 16);
+            this.comboBoxLayer.Location = new System.Drawing.Point(0, 0);
             this.comboBoxLayer.Name = "comboBoxLayer";
-            this.comboBoxLayer.Size = new System.Drawing.Size(111, 21);
-            this.comboBoxLayer.TabIndex = 4;
+            this.comboBoxLayer.Size = new System.Drawing.Size(110, 21);
+            this.comboBoxLayer.TabIndex = 0;
             this.comboBoxLayer.SelectedIndexChanged += new System.EventHandler(this.comboBoxLayer_SelectedIndexChanged);
             // 
             // btnFilter
             // 
-            this.btnFilter.Location = new System.Drawing.Point(464, 71);
+            this.btnFilter.Location = new System.Drawing.Point(437, 161);
             this.btnFilter.Name = "btnFilter";
             this.btnFilter.Size = new System.Drawing.Size(75, 23);
             this.btnFilter.TabIndex = 5;
@@ -84,27 +94,134 @@
             // 
             // panelQuery
             // 
+            this.panelQuery.AutoSize = true;
+            this.panelQuery.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelQuery.Controls.Add(this.btnAddCondition);
             this.panelQuery.Controls.Add(this.comboBoxLayer);
             this.panelQuery.Controls.Add(this.comboBoxAttributes);
             this.panelQuery.Controls.Add(this.comboBoxQuery);
             this.panelQuery.Controls.Add(this.textBoxValue);
             this.panelQuery.Location = new System.Drawing.Point(12, 12);
             this.panelQuery.Name = "panelQuery";
-            this.panelQuery.Size = new System.Drawing.Size(536, 53);
+            this.panelQuery.Size = new System.Drawing.Size(503, 52);
             this.panelQuery.TabIndex = 6;
+            // 
+            // btnAddCondition
+            // 
+            this.btnAddCondition.Location = new System.Drawing.Point(392, 26);
+            this.btnAddCondition.Name = "btnAddCondition";
+            this.btnAddCondition.Size = new System.Drawing.Size(108, 23);
+            this.btnAddCondition.TabIndex = 5;
+            this.btnAddCondition.Text = "Add Condition";
+            this.btnAddCondition.UseVisualStyleBackColor = true;
+            this.btnAddCondition.Click += new System.EventHandler(this.btnAddCondition_Click_1);
+            // 
+            // panelQuery2
+            // 
+            this.panelQuery2.AutoSize = true;
+            this.panelQuery2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelQuery2.Controls.Add(this.btnAddCondition2);
+            this.panelQuery2.Controls.Add(this.comboBox1);
+            this.panelQuery2.Controls.Add(this.comboBox2);
+            this.panelQuery2.Controls.Add(this.comboBox3);
+            this.panelQuery2.Controls.Add(this.textBox1);
+            this.panelQuery2.Enabled = false;
+            this.panelQuery2.Location = new System.Drawing.Point(12, 103);
+            this.panelQuery2.Name = "panelQuery2";
+            this.panelQuery2.Size = new System.Drawing.Size(503, 52);
+            this.panelQuery2.TabIndex = 7;
+            // 
+            // btnAddCondition2
+            // 
+            this.btnAddCondition2.Location = new System.Drawing.Point(392, 26);
+            this.btnAddCondition2.Name = "btnAddCondition2";
+            this.btnAddCondition2.Size = new System.Drawing.Size(108, 23);
+            this.btnAddCondition2.TabIndex = 5;
+            this.btnAddCondition2.Text = "Add Condition";
+            this.btnAddCondition2.UseVisualStyleBackColor = true;
+            this.btnAddCondition2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(110, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(130, 0);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 0;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(270, 0);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(81, 21);
+            this.comboBox3.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(370, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // relationComboBox
+            // 
+            this.relationComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.relationComboBox.Enabled = false;
+            this.relationComboBox.FormattingEnabled = true;
+            this.relationComboBox.Items.AddRange(new object[] {
+            "Contains",
+            "Within",
+            "Intersect",
+            "In Distance"});
+            this.relationComboBox.Location = new System.Drawing.Point(140, 71);
+            this.relationComboBox.Name = "relationComboBox";
+            this.relationComboBox.Size = new System.Drawing.Size(83, 21);
+            this.relationComboBox.TabIndex = 8;
+            // 
+            // spatialQueryCheckBox
+            // 
+            this.spatialQueryCheckBox.AutoSize = true;
+            this.spatialQueryCheckBox.Location = new System.Drawing.Point(13, 73);
+            this.spatialQueryCheckBox.Name = "spatialQueryCheckBox";
+            this.spatialQueryCheckBox.Size = new System.Drawing.Size(121, 17);
+            this.spatialQueryCheckBox.TabIndex = 9;
+            this.spatialQueryCheckBox.Text = "Enable spatial query";
+            this.spatialQueryCheckBox.UseVisualStyleBackColor = true;
+            this.spatialQueryCheckBox.CheckedChanged += new System.EventHandler(this.spatialQueryCheckBox_CheckedChanged);
             // 
             // DefinitonQueryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(560, 113);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(526, 208);
+            this.Controls.Add(this.spatialQueryCheckBox);
+            this.Controls.Add(this.relationComboBox);
+            this.Controls.Add(this.panelQuery2);
             this.Controls.Add(this.panelQuery);
             this.Controls.Add(this.btnFilter);
             this.Name = "DefinitonQueryForm";
             this.Text = "DefinitonQueryForm";
+            this.Load += new System.EventHandler(this.DefinitonQueryForm_Load);
             this.panelQuery.ResumeLayout(false);
             this.panelQuery.PerformLayout();
+            this.panelQuery2.ResumeLayout(false);
+            this.panelQuery2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -116,5 +233,14 @@
         private System.Windows.Forms.ComboBox comboBoxLayer;
         private System.Windows.Forms.Button btnFilter;
         private System.Windows.Forms.Panel panelQuery;
+        private System.Windows.Forms.Button btnAddCondition;
+        private System.Windows.Forms.Panel panelQuery2;
+        private System.Windows.Forms.Button btnAddCondition2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox relationComboBox;
+        private System.Windows.Forms.CheckBox spatialQueryCheckBox;
     }
 }
--- SportActivities/Forms/DefinitonQueryForm.cs
+++ SportActivities/Forms/DefinitonQueryForm.cs
@@ -25,12 +25,17 @@ namespace SportActivities.Forms
         private Query query;
         private bool isQueryDefined;
 
+        private int extraConditions1;
+        private int extraConditions2;
+
         public DefinitonQueryForm()
         {
             InitializeComponent();
 
+            extraConditions1 = extraConditions2 = 0;
+
             populateLayersCombobox();
-            populateAttributesCombobox();
+            populateAttributesCombobox(((ComboboxItem)comboBoxLayer.SelectedItem).Value.ToString(), extraConditions1);
             populateConditionsCombobox();
 
             isQueryDefined = false;
@@ -50,19 +55,27 @@ namespace SportActivities.Forms
             comboBoxLayer.SelectedItem = comboBoxLayer.Items[0];
         }
 
-        private void populateAttributesCombobox()
+        private void populateAttributesCombobox(string layer, int extraConditions)
         {
             comboBoxAttributes.Items.Clear();
+            
+            List<object> attrs = DataManagement.Instance.getAllLayerAttributes(layer);
 
-            List<object> attrs = DataManagement.Instance
-                .getAllLayerAttributes(((ComboboxItem)comboBoxLayer.SelectedItem).Value.ToString());
+            ComboboxItem[] items = new ComboboxItem[attrs.Count];
 
-            foreach(object attr in attrs)
+            for(int i = 0; i < attrs.Count; ++i)
             {
                 ComboboxItem item = new ComboboxItem();
-                item.Text = Utils.beautify(attr.ToString());
-                item.Value = attr;
-                comboBoxAttributes.Items.Add(item);
+                item.Text = Utils.beautify(attrs[i].ToString());
+                item.Value = attrs[i];
+                items[i] = item;
+            }
+
+            comboBoxAttributes.Items.AddRange(items);
+
+            for(int i = 0; i < extraConditions; ++i)
+            {
+
             }
             comboBoxAttributes.SelectedItem = comboBoxAttributes.Items[0];
         }
@@ -72,9 +85,41 @@ namespace SportActivities.Forms
             comboBoxQuery.Items.AddRange(conditions);
         }
 
-        private void btnAddCondition_Click(object sender, EventArgs e)
+        private void addCondition(Control parentPanel, int extraConditions)
         {
+            ComboBox operators = new ComboBox();
+            operators.Name = "operatorComboBox" + extraConditions;
+            operators.Items.Add("OR");
+            operators.Items.Add("AND");
+            operators.Items.Add("NOT");
+            operators.Location = new Point(60, 30 * extraConditions);
+            operators.Width = 50;
+            operators.DropDownStyle = ComboBoxStyle.DropDownList;
 
+            
+            ComboBox attributes = new ComboBox();
+            attributes.Location = new Point(130, 30 * extraConditions);
+            attributes.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            ComboBox condition = new ComboBox();
+            condition.Location = new Point(270, 30 * extraConditions);
+            condition.Items.AddRange(conditions);
+            condition.Width = 80;
+            condition.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            TextBox conditionValue = new TextBox();
+            conditionValue.Location = new Point(370, 30 * extraConditions);
+            conditionValue.Width = 130;
+
+
+            Point btnLoc = btnFilter.Location;
+            btnFilter.Location = new Point(btnLoc.X, btnLoc.Y + 30);
+
+            parentPanel.Controls.Add(operators);
+            parentPanel.Controls.Add(attributes);
+            parentPanel.Controls.Add(condition);
+            parentPanel.Controls.Add(conditionValue);
+            
         }
 
         private void btnFilter_Click(object sender, EventArgs e)
@@ -85,29 +130,35 @@ namespace SportActivities.Forms
 
             string attribute = ((ComboboxItem)comboBoxAttributes.SelectedItem).Value.ToString();
 
-            string condition = "";
-            if (comboBoxQuery.SelectedItem.Equals("Equals"))
-            {
-                condition = "cast(" + attribute + " as text) = '" + textBoxValue.Text + "'";
+            query.Condition = createCondition(attribute, textBoxValue.Text, comboBoxQuery.SelectedItem.ToString());
 
-            }
-            else if (comboBoxQuery.SelectedItem.Equals("Like"))
-            {
-                condition = "cast(" + attribute + " as text) ilike '%" + textBoxValue.Text + "%'";
-            }
-            else
-            {
-                condition = attribute + " " + comboBoxQuery.SelectedItem + " " + textBoxValue.Text;
-            }
-
-            query.Condition = condition;
             isQueryDefined = true;
             Close();
         }
 
+        private string createCondition(string attribute, string value, string sign)
+        {
+            string condition = "";
+            if (sign.Equals("Equals"))
+            {
+                condition = "cast(" + attribute + " as text) = '" + value + "'";
+
+            }
+            else if (sign.Equals("Like"))
+            {
+                condition = "cast(" + attribute + " as text) ilike '%" + value + "%'";
+            }
+            else
+            {
+                condition = attribute + " " + sign + " " + value;
+            }
+
+            return condition;
+        }
+
         private void comboBoxLayer_SelectedIndexChanged(object sender, EventArgs e)
         {
-            populateAttributesCombobox();
+            populateAttributesCombobox(((ComboboxItem)comboBoxLayer.SelectedItem).Value.ToString(), extraConditions1);
         }
 
         public Query getQuery()
@@ -120,9 +171,42 @@ namespace SportActivities.Forms
             return isQueryDefined;
         }
 
-        private void comboBoxQuery_TextUpdate(object sender, EventArgs e)
+        private void DefinitonQueryForm_Load(object sender, EventArgs e)
         {
-            comboBoxQuery.Items.Add(comboBoxQuery.Text);
+
+        }
+
+        private void btnAddCondition_Click_1(object sender, EventArgs e)
+        {
+            extraConditions1++;
+
+            addCondition(Controls.Find("panelQuery", true)[0], extraConditions1);
+
+            moveControl(btnAddCondition);
+            moveControl(panelQuery2);
+            moveControl(spatialQueryCheckBox);
+            moveControl(relationComboBox);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            extraConditions2++;
+
+            addCondition(Controls.Find("panelQuery2", true)[0], extraConditions2);
+
+            moveControl(btnAddCondition2);
+        }
+
+        private void moveControl(Control control)
+        {
+            Point loc = control.Location;
+            control.Location = new Point(loc.X, loc.Y + 30);
+        }
+
+        private void spatialQueryCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            panelQuery2.Enabled = spatialQueryCheckBox.Checked;
+            relationComboBox.Enabled = spatialQueryCheckBox.Checked;
         }
     }
 }
